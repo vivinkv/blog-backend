0c8e1f653f67b814e2e398edf7466feb4defa52c
vivinkv <vivin@spiderworks.in>
2024-09-02 16:12:43 +0530

    updated

--- scripts/migration.js
+++ scripts/migration.js
@@ -4,7 +4,17 @@ const sequelizeConfig = require("../config/sequelize.config");
 const queryInterface = sequelizeConfig.getQueryInterface();
 
 const addColumn = async () => {
-
+  await queryInterface.addConstraint('servicesection', {
+    fields: ['service_id'],
+    type: 'foreign key',
+    name: 'servicesection_service_id_fkey',
+    references: {
+      table: 'services',
+      field: 'id',
+    },
+    onDelete: 'CASCADE', // or 'SET NULL' if you prefer
+    onUpdate: 'CASCADE',
+  });
   await queryInterface.addColumn("services", "service_name", {
     type: DataTypes.STRING,
     allowNull: true, 
@@ -12,4 +22,12 @@ const addColumn = async () => {
   });
 };
 
+
+
+const down=async()=>{
+  await queryInterface.removeConstraint('servicesection', 'servicesection_service_id_fkey');
+  await queryInterface.removeColumn('services','service_name');
+}
+
+down();
 addColumn();
