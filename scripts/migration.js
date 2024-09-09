28fe862abdd00a2de36e01a87c32bf4d2ff1f56e
vivinkv <vivin@spiderworks.in>
2024-09-09 13:18:02 +0530

    update

--- scripts/migration.js
+++ scripts/migration.js
@@ -5,65 +5,65 @@ const queryInterface = sequelizeConfig.getQueryInterface();
 
 // menu module migration
 
-// const addColumn = async () => {
-//   await queryInterface.addConstraint('menus', {
-//     fields: ['parent_id'],
-//     type: 'foreign key',
-//     name: 'menus_parent_id_fkey',
-//     references: {
-//       table: 'menus',
-//       field: 'id',
-//     },
-//     onDelete: 'CASCADE', 
-//     onUpdate: 'CASCADE',
-//   });
-//   // await queryInterface.addColumn('menus','link',{
-//   //   type:DataTypes.STRING,
-//   //   allowNull:true,
-//   //   defaultValue:'/'
-//   // })
-//   await queryInterface.addColumn("menus", "parent_id", {
-//     type: DataTypes.STRING,
-//     allowNull: true, 
-//     defaultValue:'null'
-//   });
-// };
-
-
-
-// const down=async()=>{
-//   await queryInterface.removeConstraint('menus', 'menus_parent_id_fkey');
-//   await queryInterface.removeColumn('menus','parent_id');
-// }
-
-// down();
-// addColumn();
-
-
-
-//new migration
-
-const addNewColumn=async()=>{
-  await queryInterface.addColumn('user','user_id',{
-    type:DataTypes.STRING,
-    allowNull:true,
+const addColumn = async () => {
+  await queryInterface.addConstraint('menus', {
+    fields: ['parent_id'],
+    type: 'foreign key',
+    name: 'menus_parent_id_fkey',
+    references: {
+      table: 'menus',
+      field: 'id',
+    },
+    onDelete: 'CASCADE', 
+    onUpdate: 'CASCADE',
+  });
+  // await queryInterface.addColumn('menus','link',{
+  //   type:DataTypes.STRING,
+  //   allowNull:true,
+  //   defaultValue:'/'
+  // })
+  await queryInterface.addColumn("menus", "parent_id", {
+    type: DataTypes.STRING,
+    allowNull: true, 
     defaultValue:'null'
-  })
+  });
+};
 
-  await queryInterface.addColumn('bannerimg','blog_id',{
-    type:DataTypes.STRING,
-    allowNull:true,
-    defaultValue:'null'
-  })
 
-  await queryInterface.addColumn('bannerimg','image_type',{
-    type:DataTypes.ENUM('banner','thumbanil','featured','attachment','og'),
-    allowNull:true,
-    defaultValue:'banner'
-  })
 
+const down=async()=>{
+  await queryInterface.removeConstraint('menus', 'menus_parent_id_fkey');
+  await queryInterface.removeColumn('menus','parent_id');
 }
 
-addNewColumn();
+down();
+addColumn();
+
+
+
+// new migration
+
+// const addNewColumn=async()=>{
+//   await queryInterface.addColumn('user','user_id',{
+//     type:DataTypes.STRING,
+//     allowNull:true,
+//     defaultValue:'null'
+//   })
+
+//   await queryInterface.addColumn('bannerimg','blog_id',{
+//     type:DataTypes.STRING,
+//     allowNull:true,
+//     defaultValue:'null'
+//   })
+
+//   await queryInterface.addColumn('bannerimg','image_type',{
+//     type:DataTypes.ENUM('banner','thumbanil','featured','attachment','og'),
+//     allowNull:true,
+//     defaultValue:'banner'
+//   })
+
+// }
+
+// addNewColumn();
 
 
