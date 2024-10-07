f4487d1196f06a195bc5a78d80d9dd99ad2f3b84
vivinkv <vivin@spiderworks.in>
2024-10-07 09:44:40 +0530

    update seed.js

--- scripts/seed.js
+++ scripts/seed.js
@@ -7,10 +7,10 @@ const blogCommentModel = require("../models/blogComment.model");
 const pageModel = require("../models/page/page.model");
 const blogCategoryModel = require("../models/blogCategory.model");
 // const readExcelFile = require("../utils/readExcelFile");
-const path=require('path');
+const path = require("path");
 const categoryData = require("../utils/data");
 const readExcelFile = require("../utils/readExcelFile");
-const slugify=require('slugify');
+const slugify = require("slugify");
 
 async function fetchData() {
   try {
@@ -261,7 +261,7 @@ const updatePageData = async () => {
     for (const page of pages) {
       await pageModel.update(
         {
-         page_name:page.dataValues.title
+          page_name: page.dataValues.title,
         },
         {
           where: {
@@ -277,29 +277,31 @@ const updatePageData = async () => {
 
 // updatePageData();
 
-
-const addCategory=async()=>{
-
-  const categoryList=await readExcelFile("utils/THH.xlsx")
-console.log(categoryList);
-  for(const category of categoryList){
-    const findCategory=await blogCategoryModel.findByPk(category.Id.toString());
+const addCategory = async () => {
+  const categoryList = await readExcelFile("utils/THH.xlsx");
+  console.log(categoryList);
+  for (const category of categoryList) {
+    const findCategory = await blogCategoryModel.findByPk(
+      category.Id.toString()
+    );
     console.log(category.Description);
-    if(!findCategory){
+    if (!findCategory) {
       await blogCategoryModel.create({
-        id:category.Id.toString(),
-        name:category.CategoryName,
-        title:category.PageTitle,
-        description:category.Description==undefined ? category.PageTitle : category.Description,
-        status:category.Status,
-        slug:category.Slub,
-        meta_title:category.MetaTitle,
-        meta_description:category.Description
+        id: category.Id.toString(),
+        name: category.CategoryName,
+        title: category.PageTitle,
+        description:
+          category.Description == undefined
+            ? category.PageTitle
+            : category.Description,
+        status: category.Status,
+        slug: category.Slub,
+        meta_title: category.MetaTitle,
+        meta_description: category.Description,
       });
     }
-    
   }
-  
+
   // for(const category of categoryData.ResourceCategories){
   //   await blogCategoryModel.create({
   //     id:category.Id.toString(),
@@ -312,37 +314,160 @@ console.log(categoryList);
   //     meta_description:category.Description
   //   });
   // }
-}
+};
 // addCategory();
 
-const addSlugs=async()=>{
-  const blogs=await blogModel.findAll({
-    where:{
-     slug:null
-    }
-  })
+const addSlugs = async () => {
+  const blogs = await blogModel.findAll({
+    where: {
+      slug: null,
+    },
+  });
 
-  for(const blog of blogs){
-    await blogModel.update({
-      slug:slugify(blog.dataValues.title,{
-        replacement:"-",
-        remove:undefined,
-        lower:true,
-        strict:false,
-        trim:true
-      })
-    },{
-      where:{
-        id:blog.dataValues.id
+  for (const blog of blogs) {
+    await blogModel.update(
+      {
+        slug: slugify(blog.dataValues.title, {
+          replacement: "-",
+          remove: undefined,
+          lower: true,
+          strict: false,
+          trim: true,
+        }),
+      },
+      {
+        where: {
+          id: blog.dataValues.id,
+        },
       }
-    })
+    );
   }
 
-  console.log('Slug Added Successfully');
-}
-addSlugs();
+  console.log("Slug Added Successfully");
+};
+// addSlugs();
 
+const addBlogs = async () => {
+  const blogs = await readExcelFile("utils/blog.xlsx");
+  let not_working = [];
+  let working = [];
+  for (let i = 2; i < blogs.length; i++) {
+    const id = blogs[i].__EMPTY_1.split("/").pop().split("-")[0];
 
+    const findBlog = await blogModel.findByPk(id);
+    if (!findBlog) {
+      const blogDetails = await axios.get(
+        `http://www.thehappyhomes.com/getresources.aspx?MaxCount=1&StartId=${id}`
+      );
 
+      if (Array.isArray(blogDetails?.data)) {
+        console.log("working");
+        console.log(blogDetails.data?.length);
+       
+         
+            // console.log(i);
+            // console.log(typeof blog.ID.toString());
+            console.log(blogDetails.data[0].Member.ID);
+            console.log(blogDetails.data[0].ID);
+            let user = await userModel.findOne({
+              where: {
+                email: blogDetails.data[0].Member.Email,
+              },
+            });
 
+            if (!user) {
+              user = await userModel.create({
+                id: blogDetails.data[0].Member.ID.toString(),
+                name: blogDetails.data[0].Member.Name,
+                email: blogDetails.data[0].Member.Email,
+                password: blogDetails.data[0].Member.Passcrypt,
+                user_id: blogDetails.data[0].Member.UserID,
+              });
+            }
 
+            const findBlog = await blogModel.findByPk(blogDetails.data[0].ID.toString());
+            if (!findBlog) {
+              const newBlog = await blogModel.create({
+                id: blogDetails.data[0].ID.toString(),
+                title: blogDetails.data[0].Title,
+                meta_title: blogDetails.data[0].MetaTitle,
+                short_description: blogDetails.data[0].MetaDescription,
+                meta_description: blogDetails.data[0].MetaDescription,
+                description: blogDetails.data[0].Description,
+                author: user.dataValues.id,
+                publish_date: blogDetails.data[0].PostedDate,
+                is_published: blogDetails.data[0].PublishStatus,
+              });
+
+              for (attachment of blogDetails.data[0].Attachments) {
+                await bannerImageModel.create({
+                  blog_id: newBlog.dataValues.id,
+                  resource_id: attachment.ResourceId,
+                  fieldname: attachment.FileType,
+                  originalname: attachment.Filename,
+                  encoding: attachment.PostedMemberId,
+                  mimetype: attachment.PostedMemberId,
+                  destination: attachment.Filename,
+                  filename: attachment.Filename,
+                  path: attachment.Filename,
+                  size: attachment.ResourceId,
+                  image_type: "attachment",
+                });
+              }
+            }
+
+            for (const comment of blogDetails.data[0].Responses) {
+              let user = await userModel.findOne({
+                where: {
+                  email: comment.Email,
+                },
+              });
+
+              if (!user) {
+                user = await userModel.create({
+                  email: comment.Email,
+                  password: uuidv4(),
+                  name: comment.AuthorName,
+                  user_id: comment.Email,
+                });
+              } else {
+                await userModel.update(
+                  {
+                    user_id: comment.Email,
+                  },
+                  {
+                    where: {
+                      id: user.dataValues.id,
+                    },
+                  }
+                );
+              }
+
+              const findComment = await blogCommentModel.findOne({
+                where: {
+                  comment: comment.Description,
+                  blog_id: comment.ResourceId.toString(),
+                },
+              });
+
+              if (!findComment) {
+                const createComment = await blogCommentModel.create({
+                  comment: comment.Description,
+                  user_id: user.dataValues.id,
+                  blog_id: comment.ResourceId,
+                  createdAt: comment.PostedDate,
+                  status: comment.Status,
+                });
+              }
+            }
+          
+        
+      } else {
+        not_working.push(id);
+      }
+    }
+  }
+  console.log("Data Stored Successfully");
+};
+
+addBlogs();
